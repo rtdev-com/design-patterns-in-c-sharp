ed062d89fbef9fbb5bc40f42247cb3ecdb9da8f5
rtdev-com <81729714+rtdev-com@users.noreply.github.com>
2022-12-01 20:41:54 -0500

    chapter3 code working

--- chapter3/Program.cs
+++ chapter3/Program.cs
@@ -5,7 +5,7 @@ namespace starbuzz
     {
         /* An abstract class with cost implemented in
         subclasses */
-        string Description { get; } = "Unknown Beverage";
+        public virtual string Description { get; protected set; } = "Unknown Beverage";
 
         public abstract double Cost();
     }
@@ -15,9 +15,133 @@ namespace starbuzz
         Beverage beverage;
 
         // Ensure that decorators implement GetDescription
-        public abstract string GetDescription();
+        public abstract override string Description { get; }
     }
 
-    /**** STEP 2 ****/
-    // TO DO
+    /**** STEP 2 - Beverages ****/
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            Description = "Espresso";
+        }
+
+        public override double Cost()
+        {
+            return 1.99;
+        }
+    }
+
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            Description = "House Blend Coffee";
+        }
+
+
+        public override double Cost()
+        {
+            return 0.89;
+        }
+    }
+
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            Description = "Dark Roast Coffee";
+        }
+
+
+        public override double Cost()
+        {
+            return 0.99;
+        }
+    }
+
+    /* I am omitting a decaf because no one would order it */
+
+    /**** STEP 3 - Condiments ****/
+    public class Mocha : CondimentDecorator
+    {
+        Beverage _beverage;
+        // Recall: CondimentDecorator extends Beverage class
+        public Mocha(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string Description
+        {
+            get { return _beverage.Description + ", Mocha"; }
+        }
+
+        public override double Cost()
+        {
+            return _beverage.Cost() + 0.20;
+        }
+    }
+
+    public class Soy : CondimentDecorator
+    {
+        Beverage _beverage;
+
+        public Soy(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string Description
+        {
+            get { return _beverage.Description + ", Soy"; }
+        }
+
+        public override double Cost()
+        {
+            return _beverage.Cost() + 0.15;
+        }
+    }
+
+    public class Whip : CondimentDecorator
+    {
+        Beverage _beverage;
+
+        public Whip(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string Description
+        {
+            get { return _beverage.Description + ", Whip"; }
+        }
+
+        public override double Cost()
+        {
+            return _beverage.Cost() + 0.10;
+        }
+    }
+
+    /**** STEP 4 ****/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage beverage = new Espresso();
+            Console.WriteLine($"{beverage.Description} ${beverage.Cost()}");
+
+            Beverage beverage1 = new DarkRoast();
+            beverage1 = new Mocha(beverage1);
+            beverage1 = new Mocha(beverage1);
+            beverage1 = new Whip(beverage1);
+            Console.WriteLine($"{beverage1.Description} ${beverage1.Cost()}");
+
+            Beverage beverage2 = new HouseBlend();
+            beverage2 = new Soy(beverage2);
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Whip(beverage2);
+            Console.WriteLine($"{beverage2.Description} ${beverage2.Cost()}");
+        }
+    }
 }
--- chapter3/README.md
+++ chapter3/README.md
@@ -9,4 +9,7 @@ subclassing for extended functionality (p 91)
 
 ## Program Output:
 ```
+Espresso $1.99
+Dark Roast Coffee, Mocha, Mocha, Whip $1.49
+House Blend Coffee, Soy, Mocha, Whip $1.34
 ```
