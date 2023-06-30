3acd109c57060ee98b0e88148cc247ed22804bfd
rtdev-com <81729714+rtdev-com@users.noreply.github.com>
2023-06-30 16:38:44 -0400

    The Facade pattern implementation

---
+++ chapter7/Facade/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace TestingTheFacade
+{
+    /**** STEP 1 - Define Facade ****/
+    public class HomeTheaterFacade
+    {
+        private TV _tvComponent;
+        private Stereo _stereoComponent;
+
+        public HomeTheaterFacade()
+        {
+            _tvComponent = new TV();
+            _stereoComponent = new Stereo();
+        }
+
+        public void WatchMovie()
+        {
+            _tvComponent.On();
+            _stereoComponent.On();
+            _stereoComponent.Mode = "AUX";
+            _tvComponent.Play();
+        }
+
+        public void EndMovie()
+        {
+            _tvComponent.Off();
+            _stereoComponent.Off();
+        }
+        
+        public void PlayRadio()
+        {
+            _stereoComponent.On();
+            _stereoComponent.Mode = "FM";
+        }
+
+        public void EndRadio()
+        {
+            _stereoComponent.Off();
+        }
+    }
+
+    /**** STEP 2 - Define Components ****/
+    public class TV
+    {
+        public void On()
+        {
+            Console.WriteLine("The TV is ON");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("The TV is playing a movie");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("The TV is OFF");
+        }
+    }
+
+    public class Stereo
+    {
+        private string _mode = "AM";
+        public string Mode 
+        { 
+            get { return _mode; }
+            set 
+            {
+                _mode = value;
+                Console.WriteLine($"The Stereo mode is set to {Mode}");
+            }
+        }
+
+        public void On()
+        {
+            Console.WriteLine("The Stereo is ON");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("The Stereo is OFF");
+        }       
+    }
+    
+    /**** STEP 3 ****/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeTheaterFacade facade = new HomeTheaterFacade();
+
+            Console.WriteLine("Testing the Home Theater...");
+            facade.WatchMovie();
+            facade.EndMovie();
+
+            Console.WriteLine("\nTesting the Radio...");
+            facade.PlayRadio();
+            facade.EndRadio();
+        }
+    }
+}
---
+++ chapter7/Facade/README.md
@@ -0,0 +1,21 @@
+# Chapter 7
+For Facade Pattern, start reading on page 266
+
+## Factory Pattern
+* Provides a unified interface to a set of interfaces in a subsystem. Facade defines a higher-level interface that makes the subsystem easier to use. (p 266)
+
+## Program Output:
+```
+Testing the Home Theater...
+The TV is ON
+The Stereo is ON
+The Stereo mode is set to AUX
+The TV is playing a movie
+The TV is OFF
+The Stereo is OFF
+
+Testing the Radio...
+The Stereo is ON
+The Stereo mode is set to FM
+The Stereo is OFF
+```
